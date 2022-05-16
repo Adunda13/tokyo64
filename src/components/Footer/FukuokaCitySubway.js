26f926957f83a781cddeae6d8e53d1709eae89e3
Adunda13 <64tokyo@gmail.com>
2022-05-16 13:38:22 -0700

    updated file

--- src/components/Footer/FukuokaCitySubway.js
+++ src/components/Footer/FukuokaCitySubway.js
@@ -1,6 +1,6 @@
 import { Container} from 'react-bootstrap';
 import React from 'react';
-import FukuokaCitySubwayLogo from './fukuokaCitySubway.jpg';
+import FukuokaCitySubwayLogo from 'https://s3.console.aws.amazon.com/s3/object/tokyo64.com?region=us-west-2&prefix=fukuokaCitySubway.jpg';
 
 const FukuokaCitySubway = () => {
   
