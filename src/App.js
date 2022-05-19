6c0972728a7a163070c7c1f7469cb0e9ead0b0c5
Adunda13 <64tokyo@gmail.com>
2022-05-19 14:00:33 -0700

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'https://s3.us-west-2.amazonaws.com/tokyo64.com/logo.jpg'}  alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container, Nav,Navbar, NavDropdown,Button,Form,FormControl} from 'react-bootstrap';
-import logo from '../../Logo.js';
+import logo from './Logo.js';
 
 const Navbox = () => {
 return (
