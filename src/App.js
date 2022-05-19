0edff33613f1707a642dc8f71c68b8b1540aa43f
Adunda13 <64tokyo@gmail.com>
2022-05-19 14:55:50 -0700

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Home from './pages';
-import About from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
 
 function App() {
   return (
    <Router>
      <Navbar />
      <Routes>
-     <Route path='/'  exact component={Home} />
-     <Route path='/about' component={About} />
+      <Route path='/'  exact component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
      </Routes>
    </Router>
   );
---
+++ src/pages/contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Container } from 'react-bootstrap';
+
+const Contact = () => {
+return (
+	<div>
+	<Container className="containerPadding">
+
+	<h1 className="headerStyle">Contact Us</h1>
+	<hr className="tsLine" />
+	<p>More to come.</p>
+
+
+	</Container>
+	</div>
+);
+};
+
+export default Contact;
