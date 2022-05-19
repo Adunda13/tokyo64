59cfa3c58dca0fabba9d53ac219011d4675669f5
Adunda13 <64tokyo@gmail.com>
2022-05-19 15:01:22 -0700

    removed new pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.scss';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Home from './pages';
-import About from './pages/about';
-import Contact from './pages/contact';
+//import About from './pages/about';
+//import Contact from './pages/contact';
 
 function App() {
   return (
@@ -11,8 +11,6 @@ function App() {
      <Navbar />
      <Routes>
       <Route path='/'  exact component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/contact' component={Contact} />
      </Routes>
    </Router>
   );
