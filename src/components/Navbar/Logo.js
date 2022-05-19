b3d0d676de439465e543d2e7a9b0843e40f370d1
Adunda13 <64tokyo@gmail.com>
2022-05-19 14:33:06 -0700

    updated Logo file

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from 'https://s3.us-west-2.amazonaws.com/tokyo64.com/logo.jpg';
 
 function Logo() {
     return (
         <div>
-        <img className="logoBox" src={logo} alt="Tokyo 64 logo"/>
+        <img className="logoBox" src={'https://s3.us-west-2.amazonaws.com/tokyo64.com/logo.jpg'} alt="Tokyo 64 logo"/>
         <a
           className="App-link"
           href="https://www.tokyo64.com"
