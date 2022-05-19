7db0db569ba8a781879214b88263c84b79075971
Adunda13 <64tokyo@gmail.com>
2022-05-19 13:52:06 -0700

    added logo image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'https://s3.us-west-2.amazonaws.com/tokyo64.com/logo.jpg'}  alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
